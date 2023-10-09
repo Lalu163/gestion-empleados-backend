bb5813a3030d9178bebb33b12fcc8333c459da13
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-10-09 21:54:37 +0200

    añadiendo anotación cross origins al controller

--- src/main/java/com/gestion/empleados/gestionempleadosbackend/controllers/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestionempleadosbackend/controllers/EmpleadoController.java
@@ -2,6 +2,7 @@ package com.gestion.empleados.gestionempleadosbackend.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.gestion.empleados.gestionempleadosbackend.repositories.EmpleadoReposi
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoController {
 
     @Autowired
