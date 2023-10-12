31243a3b7cb088f2aff631594f4e1a820cf29836
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-10-12 17:33:34 +0200

    añadido post mapping

--- src/main/java/com/gestion/empleados/gestionempleadosbackend/controllers/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestionempleadosbackend/controllers/EmpleadoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,9 @@ public class EmpleadoController {
         return repositorio.findAll();
     }
     
+    @PostMapping("/empleados")
+    public Empleado guardarEmpleado(@RequestBody Empleado empleado){
+        return repositorio.save(empleado);
+    }
+
 }
